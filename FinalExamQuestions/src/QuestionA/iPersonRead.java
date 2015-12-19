7b56ef8a694411cd18da8e5678329a43eefec45d
Darsh Patel <144890dpatel@gmail.com>
2015-12-19 07:53:37 -0500

    final

---
+++ FinalExamQuestions/src/QuestionA/iPersonRead.java
@@ -0,0 +1,23 @@
+package QuestionA;
+
+import java.util.Date;
+
+public interface iPersonRead {
+
+	String getFirstName();
+	
+	String getMiddleName();
+	
+	String getLastName();
+	
+	Date getDOB();
+	
+	String getAddress();
+	
+	String getPhone();
+	
+	String getEmail();
+	
+	
+	
+}
---
+++ FinalExamQuestions/src/QuestionC/IllegalRectangle.java
@@ -0,0 +1,17 @@
+package QuestionC;
+
+public class IllegalRectangle extends Exception{
+	private int x;
+	private int y;
+
+	public int IllegalRectangle(int x1, int y1){
+		this.x1 = x;
+		this.y1 = y;
+	}
+	double getx(){
+		return x;
+	}
+	double gety(){
+		return y;
+	}
+}
